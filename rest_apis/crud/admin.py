c231e5181d3c3a248845538a718833d848622a48
omersaleem09 <omersaleem09@hotmail.com>
2024-02-04 01:11:34 +0500

    Registered the models of crud application Implemented functionality to list and search as per the requriement

--- rest_apis/crud/admin.py
+++ rest_apis/crud/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Patient, Counsellor, Appointment
 
-# Register your models here.
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'is_active']
+    search_fields = ['name', 'email']
+    list_filter = ['is_active']
+
+class CounsellorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'is_active']
+    search_fields = ['name', 'email']
+    list_filter = ['is_active']
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'patient', 'counsellor', 'appointment_date', 'is_active']
+    search_fields = ['patient__name', 'counsellor__name', 'appointment_date']
+    list_filter = ['is_active']
+
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(Counsellor, CounsellorAdmin)
+admin.site.register(Appointment, AppointmentAdmin)
